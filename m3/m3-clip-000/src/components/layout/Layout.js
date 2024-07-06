c844a65e5283dd819670e734443b3260ed20c875
Andrew Haywood <andrew.haywood@hotmail.com>
2024-07-06 11:49:51 +0100

    Finsihed context

--- m3/m3-clip-000/src/components/layout/Layout.js
+++ m3/m3-clip-000/src/components/layout/Layout.js
@@ -1,15 +1,22 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import { useContext } from "react";
+import { ThemeContext } from "../../App";
 
 const Layout = (props) => {
-  const layoutVersion = "Version 2.0"
+  const layoutVersion = "Version 2.0";
+  // destrutres the ThemeContext objext gettign the darktheme
+  const { darkTheme } = useContext(ThemeContext);
   return (
-    <div className="container" data-theme={props.darkTheme ? "dark" : "light"}>
+    <div
+      className="container"
+      data-theme={darkTheme ? "dark" : "light"}
+    >
       {/* <Header toggleTheme={props.toggleTheme} darkTheme={props.darkTheme} /> */}
-      {/* Use the spread operator instead of above 
-      * You can also add local dat created in the component a well as the props passed in
-      */}
-      <Header {...props} layoutVersion={layoutVersion} />
+      {/* Use the spread operator instead of above
+       * You can also add local dat created in the component a well as the props passed in
+       */}
+      <Header layoutVersion={layoutVersion} />
       {props.children}
       <br />
       <Footer />
